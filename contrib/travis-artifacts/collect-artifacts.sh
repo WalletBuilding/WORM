46554301101a65c4ea473906835787622b61497d
WalletBuilding <45883987+WalletBuilding@users.noreply.github.com>
2019-03-19 01:29:13 +0100

    Update collect-artifacts.sh

--- contrib/travis-artifacts/collect-artifacts.sh
+++ contrib/travis-artifacts/collect-artifacts.sh
@@ -1,5 +1,3 @@
-#!/bin/bash -ex
-
 if [ $# -lt 5 ] ; then
     echo "Usage: $0 basedir outdir releasedir commit host"
     exit 1
@@ -13,25 +11,4 @@ HOST=$5
 
 RELEASEDIR=$BASEDIR/$RELEASEBASE/$COMMIT/$HOST
 
-cd $BASEDIR/worm-$HOST
-
-mkdir -p $RELEASEDIR
-#ZIPFILES=$(ls $OUTDIR/bin/* || true)
-
-echo "make zip file"
-cp -r $OUTDIR/bin/* $RELEASEDIR/
-#zip -r $RELEASEDIR/bitcoin-$COMMIT.zip $OUTDIR
-
-# Linux artifacts
-#[ -z "$ZIPFILES" ] || \
-#       zip -uj $RELEASEDIR/bitcoin-$COMMIT.zip ${ZIPFILES}
-
-#echo "copy mac qt to zip file"
-
-# MaxOSX artifacts
-#cp -a *.dmg $RELEASEDIR || true
-#echo "copy win qt to zip file"
-# Windows artifacts
-#cp -a *.exe $RELEASEDIR || true
-
-find $RELEASEDIR
+tar -cvzf package-$HOST.tgz $BASEDIR/out
