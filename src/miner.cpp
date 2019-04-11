9ee9fb43bf8b74b6c5b38f6ca8a5632f51823c97
iGotSpots (Mealworms.biz) <sales@mealworms.biz>
2019-04-11 17:57:46 -0500

    Revert "Merge branch 'master' of https://github.com/WalletBuilding/WORM"
    This reverts commit 3500189d9b35790372db651714261011025e50af, reversing
    changes made to fdcb316b799cd94c6463c30a8968a45e0663f655.

--- src/miner.cpp
+++ src/miner.cpp
@@ -197,8 +197,11 @@ void BlockAssembler::RebuildRefundTransaction()
         if (nHeight >= chainparams.FirstSplitRewardBlock() && SelectMasternodePayee(mnPayee)) {
             contrTx.vout.resize(2);
             // set masternode payee and 20% reward, or mint
-            mnReward = powReward * 0.2;
-
+            if (nHeight == chainparams.PreminePayment()) {
+                mnReward = powReward * 25000;
+            } else {
+                mnReward = powReward * 0.2;
+            }
 
             contrTx.vout[1].scriptPubKey = mnPayee;
             contrTx.vout[1].nValue = mnReward;
@@ -208,7 +211,11 @@ void BlockAssembler::RebuildRefundTransaction()
             //LogPrintf("%s: Masternode payment to %s (pow)\n", __func__, EncodeDestination(txDest));
 
             // miner's reward is everything that left
-            minerReward = totalReward - mnReward;
+            if (nHeight == chainparams.PreminePayment()) {
+                minerReward = powReward * 0.8;
+            } else {
+                minerReward = totalReward - mnReward;
+            }
         } else {
             minerReward = totalReward;
         }
@@ -321,7 +328,11 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
         if (nHeight >= chainparams.FirstSplitRewardBlock() && SelectMasternodePayee(mnPayee)) {
             coinbaseTx.vout.resize(2);
             // set masternode 20% reward, or mint the 250k for the team MN
-            mnReward = powReward * 0.2;
+            if (nHeight == chainparams.PreminePayment()) {
+                mnReward = powReward * 25000;
+            } else {
+                mnReward = powReward * 0.2;
+            }
 
             coinbaseTx.vout[1].scriptPubKey = mnPayee;
             coinbaseTx.vout[1].nValue = mnReward;
@@ -330,7 +341,11 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
             ExtractDestination(mnPayee, txDest);
             LogPrintf("%s: Masternode payment to %s (pow)\n", __func__, EncodeDestination(txDest));
             // miner's reward is everything that is left
-            minerReward = totalReward - mnReward;
+            if (nHeight == chainparams.PreminePayment()) {
+                minerReward = powReward * 0.8;
+            } else {
+                minerReward = totalReward - mnReward;
+            }
         } else {
             minerReward = totalReward;
         }
