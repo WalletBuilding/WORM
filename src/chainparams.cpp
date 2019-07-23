c078250cee093c46428b58ef7e79b4c6fdbaf3ce
iGotSpots (Mealworms.biz) <sales@mealworms.biz>
2019-07-23 14:26:08 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -197,7 +197,7 @@ public:
 		vSeeds.push_back(CDNSSeedData("92.60.44.72", "92.60.44.72"));
 		vSeeds.push_back(CDNSSeedData("95.88.47.89", "95.88.47.89"));
 		vSeeds.push_back(CDNSSeedData("95.216.247.54", "95.216.247.54"));
-		vSeeds.push_back(CDNSSeedData("104.238.176.80", "104.238.176.80")); 
+		vSeeds.push_back(CDNSSeedData("104.238.176.80", "104.238.176.80"));
 		vSeeds.push_back(CDNSSeedData("108.31.235.3", "108.31.235.3")); 
 		vSeeds.push_back(CDNSSeedData("108.77.80.39", "108.77.80.39")); 
 		vSeeds.push_back(CDNSSeedData("110.138.68.221", "110.138.68.221")); 
