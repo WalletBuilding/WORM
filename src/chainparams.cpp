b09a01c72ed39a4f1fb0ce7910ea6d7b48b3d8c8
iGotSpots (Mealworms.biz) <sales@mealworms.biz>
2019-07-23 14:26:49 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -204,7 +204,7 @@ public:
 		vSeeds.push_back(CDNSSeedData("125.212.181.159", "125.212.181.159"));
 		vSeeds.push_back(CDNSSeedData("182.0.137.195", "182.0.137.195"));
 		vSeeds.push_back(CDNSSeedData("185.239.239.179", "185.239.239.179"));
-		vSeeds.push_back(CDNSSeedData("210.195.255.223", "210.195.255.223")); 
+		vSeeds.push_back(CDNSSeedData("210.195.255.223", "210.195.255.223"));
 		vSeeds.push_back(CDNSSeedData("213.108.119.84", "213.108.119.84")); 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,73); // WORM address start with 'W'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,63); // WORM script addresses start with 'S'
